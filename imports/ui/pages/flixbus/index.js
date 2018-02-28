f7ac34f550eda2ff35a9ca142a03a8ba9fae08f2
Romeo Bellon <romeo.bellon@me.com>
2018-02-28 19:35:13 +0800

    Fix already redeemed promo codes

--- imports/ui/pages/flixbus/index.js
+++ imports/ui/pages/flixbus/index.js
@@ -2,8 +2,7 @@ import './flixbus.html'
 import Flixbus from '/imports/collections/flixbus';
 
 Template.FlixbusPage.onCreated(function () {
-  let notRedeemed = !localStorage.getItem('redeemed') || !localStorage.getItem('redeemed');
-  this.showCodes = new ReactiveVar(!notRedeemed);
+  this.showCodes = new ReactiveVar(false);
   this.codes = new ReactiveVar('');
   this.errors = new ReactiveVar('');
 
@@ -13,15 +12,14 @@ Template.FlixbusPage.onCreated(function () {
 Template.FlixbusPage.events({
   'click #getCodes': function (event, template) {
     event.preventDefault();
-    console.log('asd');
 
-    let f = Flixbus.find({redeemed: false}, {limit: 20}).fetch();
+    let f = Flixbus.find({redeemed: false}, {limit: 2}).fetch();
     if (f.length === 0) {
       template.errors.set('No promo codes left');
     } else {
-      console.log(f);
       localStorage.setItem('redeemed', true);
-      // template.codes.set(`${codes[0].code} ${codes[1].code}`);
+      template.showCodes.set(true);
+      template.codes.set(`${f[0].code} ${f[1].code}`);
       Flixbus.update({_id: f[0]._id}, {$set: {redeemed: true}});
       Flixbus.update({_id: f[1]._id}, {$set: {redeemed: true}});
     }
@@ -37,5 +35,8 @@ Template.FlixbusPage.helpers({
   },
   errors: function () {
     return Template.instance().errors.get();
+  },
+  alreadyRedeemed: function () {
+    return localStorage.getItem('redeemed');
   }
 });
